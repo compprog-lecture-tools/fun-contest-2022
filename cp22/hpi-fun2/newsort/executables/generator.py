111d3728880d4d6e0cd057ad659a5844c0da7817
Sandro <sandrospeh@googlemail.com>
2022-07-06 16:59:24 +0200

    renamed exercise and adapted test case generator with long time testcase

--- cp22/hpi-fun2/newsort/executables/generator.py
+++ cp22/hpi-fun2/newsort/executables/generator.py
@@ -13,16 +13,17 @@ write_testcase('sample1', 'Sample 1 easy', [1, 3, 5, 8], '0123456789')
 write_testcase('sample2', 'Sample 2 easy', [92, 53, 10], 9876543210)
 write_testcase('sample3', 'Sample 3', [47, 40, 99], 2164783059)
 write_testcase('sample4', 'Sample 4', [0, 40, 99], '0164783259')
+write_testcase('wrong_order', 'wrong order', [1, 2], 1234567890)
+write_testcase('same_values', 'numbers are the same', [1, 1], 1234567890)
+write_testcase('different_lengths', 'values have different lengths', [1, 11111], 1234567890)
+write_testcase('different_lengths2', 'values have different lengths', [11111, 1233, 15666666], 1234567890)
+
 
 MAX_N = 2 * 10**5
 MAX_VAL = 10**9
 
 write_testcase('max_input', 'Max n, max vals', [MAX_VAL] * MAX_N, 9876543210)
 write_testcase('min_input', 'Min n, min val', [0], 9876543210)
-write_testcase('wrong_order', 'wrong order', [1, 2], 1234567890)
-write_testcase('different_lengths', 'values have different lengths', [1, 2222222222], 1234567890)
-write_testcase('different_lengths2', 'values have different lengths', [11111, 1233, 15666666], 1234567890)
-
 
 def random_order():
     numArray = [];
@@ -41,3 +42,14 @@ for i in range(1, 5 + 1):
         numbersArray.append(random_value())
     write_testcase(f"random{i}", f"Random {i}", numbersArray, random_order())
 
+
+descending_order = []
+numbersArray = [];
+
+for i in range(0, MAX_N):
+    numbersArray.append(random_value())
+
+numbersArray.sort()
+write_testcase('worst runtime but positive', 'descending til the end', numbersArray, '0123456789')
+numbersArray[-1] = 0
+write_testcase('worst runtime but negative', 'descending til the end', numbersArray, '0123456789')
