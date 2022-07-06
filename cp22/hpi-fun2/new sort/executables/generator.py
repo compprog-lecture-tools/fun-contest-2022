1f35c4614fd0d09a465263578ace8372af131a6e
Sandro <sandrospeh@googlemail.com>
2022-07-06 14:07:59 +0200

    initial setup of problem with finished executables

---
+++ cp22/hpi-fun2/new sort/executables/generator.py
@@ -0,0 +1,43 @@
+import random
+from pathlib import Path
+
+random.seed(-6653597241841577263)
+
+
+def write_testcase(name, desc, numbers, newNumberOrdering):
+    Path(name + '.in').write_text(str(len(numbers)) + '\n' + ' '.join(map(str, numbers)) + '\n' + ' ' + str(newNumberOrdering))
+    Path(name + '.desc').write_text(desc + '\n')
+
+
+write_testcase('sample1', 'Sample 1 easy', [1, 3, 5, 8], '0123456789')
+write_testcase('sample2', 'Sample 2 easy', [92, 53, 10], 9876543210)
+write_testcase('sample3', 'Sample 3', [47, 40, 99], 2164783059)
+write_testcase('sample4', 'Sample 4', [0, 40, 99], '0164783259')
+
+MAX_N = 2 * 10**5
+MAX_VAL = 10**9
+
+write_testcase('max_input', 'Max n, max vals', [MAX_VAL] * MAX_N, 9876543210)
+write_testcase('min_input', 'Min n, min val', [0], 9876543210)
+write_testcase('wrong_order', 'wrong order', [1, 2], 1234567890)
+write_testcase('different_lengths', 'values have different lengths', [1, 2222222222], 1234567890)
+write_testcase('different_lengths2', 'values have different lengths', [11111, 1233, 15666666], 1234567890)
+
+
+def random_order():
+    numArray = [];
+    while(len(numArray) < 10):
+        randDigit = random.randrange(0, 9+1)
+        if(randDigit not in numArray ):
+            numArray.append(randDigit)
+    return str(''.join(map(str, numArray)))
+
+def random_value():
+    return random.randrange(0, MAX_VAL)
+
+for i in range(1, 5 + 1):
+    numbersArray = [];
+    for i in range(0, MAX_N):
+        numbersArray.append(random_value())
+    write_testcase(f"random{i}", f"Random {i}", numbersArray, random_order())
+
---
+++ cp22/hpi-fun2/new sort/executables/solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include<cmath>
+
+#define rep(a, b)   for(int a = 0; a < (b); ++a)
+#define all(a)      (a).begin(),(a).end()
+#define endl        '\n'
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    //content
+    int countOfNumbers;
+    cin >> countOfNumbers;
+    vector<string> numbers(countOfNumbers);
+    rep(i, countOfNumbers) {
+        cin >> numbers[i];
+    }
+    string digitOrder;
+    cin >> digitOrder;
+    map<char, int> charVals;
+
+    // map characters to their digitOrder
+    for (int i = 0; i < digitOrder.length(); ++i) {
+        charVals[digitOrder[i]] = i;
+    }
+
+    for (int i = 1; i < numbers.size(); ++i) {
+        string s1 = numbers[i - 1];
+        string s2 = numbers[i];
+
+        if (s1 == s2) {
+            continue;
+        }
+        if(s1.length() > s2.length()){
+            continue;
+        } else if (s1.length() < s2.length()){
+            cout << "NO";
+            cout << endl;
+            return 0;
+        }
+
+        // advance to the first non-matching digit at the same position in the two numbers
+        int len = min(s1.length(), s2.length());
+        int j = 0;
+        for (int j = 0; j < len && s1[j] == s2[j]; ++j);
+
+        // compare non-matching digits based on the ordering
+        if (charVals[s1[j]] > charVals[s2[j]]) {
+            cout << "NO";
+            cout << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES";
+    cout << endl;
+    return 0;
+}
