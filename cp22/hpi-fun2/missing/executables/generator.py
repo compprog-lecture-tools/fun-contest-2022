7ee2e2acd8b1bcc886638613d7731b15507b4ab6
frcroth <frcroth@gmail.com>
2022-07-11 15:04:44 +0200

    Add missing problem

---
+++ cp22/hpi-fun2/missing/executables/generator.py
@@ -0,0 +1,27 @@
+from asyncore import write
+import random
+from pathlib import Path
+import string
+
+random.seed(-5439687794124513171)
+
+
+def write_testcase(name, desc, string):
+    Path(name + '.in').write_text(string)
+    Path(name + '.desc').write_text(desc + '\n')
+
+max_length = 2500
+
+write_testcase("sample1", "A simple testcase", "allaoggo")
+write_testcase("sample2", "A simple testcase", "banana")
+write_testcase("sample3", "A simple testcase", "masseffect")
+some_text = "sqddbydscraqgvzfquizlmdopleiqtceyemhuyotzj"
+write_testcase("single_letter", "Only one letter", "x")
+write_testcase("long_palindrome", "A long palindrome", some_text + "".join(reversed(some_text)))
+write_testcase("no_palindromes", "A testcase without palindromes", "azbcdefgzhijklmznopqrsztuvw")
+long_text = ''.join(random.choice(string.ascii_lowercase) for _ in range(int(max_length/2)))
+write_testcase("very_long_palindrome", "A very long palindrome", long_text + "".join(reversed(long_text)))
+write_testcase("many_palindromes", "Many similar palindromes", "".join(random.choice(["anna", "otto", "iggi"]) for _ in range(int((max_length-1)/4))))
+write_testcase("random_small", "A small random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(20)))
+write_testcase("random_large_1", "A large random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(max_length)))
+write_testcase("random_large_2", "A large random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(max_length)))
---
+++ cp22/hpi-fun2/missing/executables/solution.py
@@ -0,0 +1,16 @@
+# Code adapted from https://www.geeksforgeeks.org/palindrome-partitioning-dp-17/, by rutvik-56
+
+def min_cut(a):
+    cut = [0 for i in range(len(a))]
+    palindrome = [[False for _ in range(len(a))] for _ in range(len(a))]
+    for i in range(len(a)):
+        minCut = i
+        for j in range(i + 1):
+            if (a[i] == a[j] and (i - j < 2 or palindrome[j + 1][i - 1])):      
+                palindrome[j][i] = True
+                minCut = min(minCut,0 if  j == 0 else (cut[j - 1] + 1))
+        cut[i] = minCut;  
+    return cut[len(a)-1] + 1
+ 
+print(min_cut(input()))
+     
