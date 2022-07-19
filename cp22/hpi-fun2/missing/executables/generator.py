304131e0263232f68b9435ccdfad988a0e706340
frcroth <frcroth@gmail.com>
2022-07-19 15:37:19 +0200

    Add anti-greedy test case

--- cp22/hpi-fun2/missing/executables/generator.py
+++ cp22/hpi-fun2/missing/executables/generator.py
@@ -21,7 +21,8 @@ write_testcase("long_palindrome", "A long palindrome", some_text + "".join(rever
 write_testcase("no_palindromes", "A testcase without palindromes", "azbcdefgzhijklmznopqrsztuvw")
 long_text = ''.join(random.choice(string.ascii_lowercase) for _ in range(int(max_length/2)))
 write_testcase("very_long_palindrome", "A very long palindrome", long_text + "".join(reversed(long_text)))
+write_testcase("anti_greedy", "A testcase that has a different result for a greedy algo", "abbacddcbbae")
 write_testcase("many_palindromes", "Many similar palindromes", "".join(random.choice(["anna", "otto", "iggi"]) for _ in range(int((max_length-1)/4))))
 for i in range(5):
     write_testcase(f"random_small_{i}", "A small random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(20)))
-    write_testcase("random_large_{i}", "A large random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(max_length)))
+    write_testcase(f"random_large_{i}", "A large random testcase",''.join(random.choice(string.ascii_lowercase) for _ in range(max_length)))
