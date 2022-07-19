7c4fe29b52b6911ed3f63371ec464c6da9471f70
Tarik <T4rikA@users.noreply.github.com>
2022-07-19 15:55:34 +0200

    Resolve PR feedback

--- cp22/hpi-fun2/spaces/executables/solution.py
+++ cp22/hpi-fun2/spaces/executables/solution.py
@@ -1,23 +1,26 @@
-# code adapted from: https://www.geeksforgeeks.org/word-wrap-problem-space-optimized-solution/
-
+from sre_constants import AT_END_STRING
 import sys
 def minimalRaggedText(lengths, n, lineWidth):
-    dp = [0] * n
-    dp[n - 1] = 0
-    for i in range(n - 2, -1, -1):
-        currlen = -1
-        dp[i] = sys.maxsize
-        for j in range(i, n):
-            currlen += (lengths[j] + 1)
-            if (currlen > lineWidth):
-                break
-            if (j == n - 1):
-                cost = 0
-            else:
-                cost = ((lineWidth - currlen) * (lineWidth - currlen) + dp[j + 1])
-            if (cost < dp[i]):
-                dp[i] = cost
-    print(dp[0])
+    dp = [float('inf')] * (n+1)
+    dp[0] = 0
+
+    for i in range(n):
+        currlen = 0
+        j = i
+        while (j < n and currlen + lengths[i] <= lineWidth):
+            currlen += lengths[j]
+            j+=1
+            dp[j] = min(dp[j], dp[i] + (lineWidth-currlen)**2)
+            currlen+=1
+    
+    ans = float('inf')
+    currlen = 0
+    i = n-1
+    while i >= 0 and currlen <= lineWidth:
+        ans = min(ans, dp[i+1])
+        currlen += lengths[i] + (i < n-1)
+        i-=1
+    print(ans)
 
 n = int(input())
 l = [len(i) for i in input().split()]
